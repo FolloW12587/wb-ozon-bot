6eca1b1ae8a5828fb706bbe03ae4a7e0ab33a944
follow <follow12587@gmail.com>
2025-06-17 16:22:34 +1000

    Added send message and pin message commands

---
+++ commands/send_message.py
@@ -0,0 +1,33 @@
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    ForceReply,
+)
+
+# import config
+from bot22 import bot
+
+
+async def send_message(
+    chat_id: int,
+    text: str,
+    markup: (
+        InlineKeyboardMarkup
+        | ReplyKeyboardMarkup
+        | ReplyKeyboardRemove
+        | ForceReply
+        | None
+    ),
+) -> int:
+    msg = await bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        reply_markup=markup,
+        parse_mode="markdown",
+    )
+    return msg.message_id
+
+
+async def pin_message(chat_id: int, message_id: int) -> bool:
+    return await bot.pin_chat_message(chat_id=chat_id, message_id=message_id)
